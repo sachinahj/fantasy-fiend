28af4d2950a3408c8b58e3610f6a7c186893212a
Sachin Ahuja <sachinahj@gmail.com>
2014-07-28 11:54:32 -0500

    fixed has one relationships, added json format for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,6 +9,7 @@ class PlayersController < ApplicationController
     @ks = []
     @dsts = []
     @others = []
+
     @players.each do |player|
       case player.position
       when 'QB'
@@ -27,57 +28,65 @@ class PlayersController < ApplicationController
         @others << [player.id, player.display_name]
       end
     end
+
   end
 
   def show
+
     @player = Player.find(params[:id])
     find_projections
     find_stats
-    puts "stats --------------"
-    p @stats
 
+    respond_to do |format|
+      format.html
+      format.json {render json: @player.to_json}
+    end
 
   end
 
 
   private 
+
   def find_projections
+
     @projections = nil
     case @player.position 
     when 'QB'
-      @projections = ProjectionForQb.where(player_id: @player.id)
+      @projections = @player.projection_for_qb
     when 'RB'
-      @projections = ProjectionForRb.where(player_id: @player.id)
+      @projections = @player.projection_for_rb
     when 'WR'
-      @projections = ProjectionForWr.where(player_id: @player.id)
+      @projections = @player.projection_for_wr
     when 'TE'
-      @projections = ProjectionForTe.where(player_id: @player.id)
+      @projections = @player.projection_for_te
     when 'K'
-      @projections = ProjectionForK.where(player_id: @player.id)
+      @projections = @player.projection_for_k
     else 
-      @projections = []
+      @projections = nil
     end
+
   end
+
   def find_stats
+
     @stats = nil
     case @player.position
     when 'QB'
-      @stats = StatForQb.where(player_id: @player.id)
+      @stats = @player.stat_for_qb
     when 'RB'
-      @stats = StatForRb.where(player_id: @player.id)
+      @stats = @player.stat_for_rb
     when 'WR'
-      @stats = StatForWr.where(player_id: @player.id)
+      @stats = @player.stat_for_wr
     when 'TE'
-      @stats = StatForTe.where(player_id: @player.id)
+      @stats = @player.stat_for_te
     when 'K'
-      @stats = StatForK.where(player_id: @player.id)
+      @stats = @player.stat_for_k
     when 'DST'
-      @stats = StatForD.where(player_id: @player.id)
+      @stats = @player.stat_for_d
     else 
-      @stats = []
+      @stats = nil
     end
 
   end
-
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,23 +1,45 @@
 class Player < ActiveRecord::Base 
-  has_one :projectionforqb
-  has_one :projectionforrb
-  has_one :projectionforwr
-  has_one :projectionforte
-  has_one :projectionfork
+  has_one :projection_for_qb
+  has_one :projection_for_rb
+  has_one :projection_for_wr
+  has_one :projection_for_te
+  has_one :projection_for_k
 
-  has_one :statforqb
-  has_one :statforrb
-  has_one :statforwr
-  has_one :statforte
-  has_one :statfork
-  has_one :statford
+  has_one :stat_for_qb
+  has_one :stat_for_rb
+  has_one :stat_for_wr
+  has_one :stat_for_te
+  has_one :stat_for_k
+  has_one :stat_for_d
 
   before_save :check_bye_week_and_team_name
 
   def check_bye_week_and_team_name
+
     teamObj = Team.find_by team_abbreviation: self.team_abbreviation
     self.bye_week = teamObj.bye_week
     self.team = teamObj.team
+
+  end
+
+  def to_json(options = nil)
+    if position == 'QB'
+      super(options || {include: [:stat_for_qb, :projection_for_qb]})
+    elsif position == 'RB'
+      super(options || {include: [:stat_for_rb, :projection_for_rb]})
+    elsif position == 'WR'
+      super(options || {include: [:stat_for_wr, :projection_for_wr]})
+    elsif position == 'TE'
+      super(options || {include: [:stat_for_te, :projection_for_te]})
+    elsif position == 'K'
+      super(options || {include: [:stat_for_k, :projection_for_k]})
+    elsif position == 'DST'
+      super(options || {include: :stat_for_d})
+    else
+      nil
+    end
+
+
   end
 
 end
