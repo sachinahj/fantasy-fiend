6565283d9f809b306aa26042369326ad0bdaa355
Sachin Ahuja <sachinahj@gmail.com>
2014-07-29 20:07:41 -0500

    added compare views for each position

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,48 +1,27 @@
 class CompareController < ApplicationController
-  def quarterbacks
-    @quarterbacks = Player.where(position: "QB").includes(:projection_for_qb, :stat_for_qb).limit(10)
 
-    @completions = []
-    @attempts = []
-    @yards = []
-    @touchdowns = []
-    @interceptions = [] 
-    @fantasy_points = []
-    
-    @series = []
-    @quarterbacks.each do |quarterback|
-      qb = {
-        name: quarterback.display_name,
-        data: [
-          quarterback.projection_for_qb.pass_completions,
-          quarterback.projection_for_qb.pass_attempts,
-          quarterback.projection_for_qb.pass_yards,
-          quarterback.projection_for_qb.pass_touchdowns,
-          quarterback.projection_for_qb.pass_interceptions,
-          quarterback.projection_for_qb.fantasy_points
-        ]
-      }
-      @series << qb
-    end
+  def quarterbacks 
+    @quarterbacks = Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
 
-  def runningbacks
-    @runningbacks = Player.where(position: "RB").includes(:projection_for_rb, :stat_for_rb)
+  def runningbacks 
+    @runningbacks = Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
 
-  def receivers
-    @receivers = Player.where(position: "WR").includes(:projection_for_wr, :stat_for_wr)
+  def receivers 
+    @receivers = Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
 
-  def tightends
-    @tightends = Player.where(position: "TE").includes(:projection_for_te, :stat_for_te)
+  def tightends 
+    @tightends = Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
 
-  def kickers
-    @kickers = Player.where(position: "K").includes(:projection_for_k, :stat_for_k)
+  def kickers 
+    @kickers = Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
 
   def defenses
-    @defenses = Player.where(position: "DST").includes(:stat_for_d)
+    @defenses= Player.where(position: "QB").includes(:season2013_stat, :season2014_projection)
   end
+  
 end
