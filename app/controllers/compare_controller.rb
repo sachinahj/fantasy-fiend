d49ecd036687da15c86faa06e8c01190df484826
Sachin Ahuja <sachinahj@gmail.com>
2014-07-31 12:35:08 -0500

    deleted compare#*position views

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -4,32 +4,7 @@ class CompareController < ApplicationController
   def index
   end
 
-  def laboratory
-    
-  end
-
-  def quarterbacks 
-    @quarterbacks = Player.all.where(position: "QB").where("overall_draft_position < 100").includes(:season_stat, :season_projection)
-  end
-
-  def runningbacks 
-    @runningbacks = Player.where(position: "RB").includes(:season2013_stat, :season2014_projection)
-  end
-
-  def receivers 
-    @receivers = Player.where(position: "WR").includes(:season2013_stat, :season2014_projection)
-  end
-
-  def tightends 
-    @tightends = Player.where(position: "TE").includes(:season2013_stat, :season2014_projection)
-  end
-
-  def kickers 
-    @kickers = Player.where(position: "K").includes(:season2013_stat, :season2014_projection)
-  end
-
-  def defenses
-    @defenses= Player.where(position: "DST").includes(:season2013_stat, :season2014_projection)
+  def laboratory    
   end
 
   private 
